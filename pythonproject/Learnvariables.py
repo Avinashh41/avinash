16ef63883e7036e01466d106df564f08d210a447
Avinashh41 <115032369+Avinashh41@users.noreply.github.com>
2023-01-06 19:12:00 +0530

    pro

---
+++ pythonproject/Learnvariables.py
@@ -0,0 +1,40 @@
+
+
+x = "good"
+
+def myf():
+  print ("python is" + x)
+
+myf()
+
+# use outside variable create
+
+x = "good"
+
+def myf():
+    x = "very "
+print ("python is" + x)
+
+myf()
+
+print ("python is" + x)
+
+# Create a variable inside a function, with the same name as the global variable
+
+# Numeric types
+#Int
+#Float
+#complex
+
+
+x = 10
+y = 10.3
+z = 1j
+print (x)
+print (y)
+print (z)
+print (type(x))
+print (type(y))
+print (type(z))
+
+# use numeric
