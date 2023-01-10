b901661e0acbb32d2e4b9a78f0ff1ebba0a8a5ed
Avinashh41 <115032369+Avinashh41@users.noreply.github.com>
2023-01-10 17:23:22 +0530

    pro

---
+++ projectgithub/string.py
@@ -0,0 +1,26 @@
+mystr = """I ask them to take a poem
+and hold it up to the light
+like a color slide
+                  
+or press an ear against its hive.
+                
+I say drop a mouse into a poem
+and watch him probe his way out,
+or walk inside the poem's room
+and feel the walls for a light switch.
+                  
+I want them to waterski
+across the surface of a poem
+waving at the author's name on the shore.
+        
+
+
+#print (len(mystr))
+#print (mystr. find("to"))
+#print (mystr. replace ("them", "are"))
+#print (mystr. lower())
+#print (mystr. upper())
+
+print (len(mystr))
+print (mystr.replace("They","on"))
+
